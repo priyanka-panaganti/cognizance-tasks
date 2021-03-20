4f8331edda840deafc27a58dd7fdd00f86756441
priyanka-panaganti <75232318+priyanka-panaganti@users.noreply.github.com>
2021-03-20 12:53:45 +0530

    Update task1.c

--- task-1/task1.c
+++ task-1/task1.c
@@ -1,1 +1,24 @@
+#include <stdio.h>
 
+int main()
+{
+  // declaring integers x and n as position of elephant and friend
+    int x,n;
+    int steps=0;
+    scanf( "%d" , & x);
+    if (x<=5)
+    {
+    n=1;
+    }
+    else 
+    
+    if (x % 5 == 0)
+    {
+        n= x/5 ;
+    }
+    else
+    {
+        n=x/5 + 1;
+    }
+    printf ("%d", n);
+}
