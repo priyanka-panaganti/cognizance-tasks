6f83a4969420d36f437c57b4520a00b993a1d563
priyanka-panaganti <75232318+priyanka-panaganti@users.noreply.github.com>
2021-03-20 12:42:15 +0530

    task1.c

--- task-1/task1.c
+++ task-1/task1.c
@@ -1,1 +1,23 @@
+#include <stdio.h>
 
+int main()
+{
+    int x,n;
+    int steps=0;
+    scanf( "%d" , & x);
+    if (x<=5)
+    {
+    n=1;
+    }
+    else 
+    
+    if (x % 5 == 0)
+    {
+        n= x/5 ;
+    }
+    else
+    {
+        n=x/5 + 1;
+    }
+    printf ("%d", n);
+}
